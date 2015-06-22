1f0be89f363d580dffab6da9a250ce8209144eb8
Sunjoo Park <allessunjoo.park@lge.com>
2015-06-22 19:25:00 +0900

    Initial Commit

---
+++ TestProject2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestProject2
+//
+//  Created by Sunjoo Park on 6/22/15.
+//  Copyright (c) 2015 Sunjoo Park. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
